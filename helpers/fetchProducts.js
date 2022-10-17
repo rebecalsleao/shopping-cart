2afad7455527395b4f6fc3c516a0afb5464f51bf
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-17 20:49:20 -0300

    Projeto - Requisito 2 ajuste

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,6 @@
 const fetchProducts = async (product) => {
   if (product === undefined) {
-    return new Error('You must provide an url');
+    throw new Error('You must provide an url');
   }
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
   const result = await response.json();
